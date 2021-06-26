d99b4e66068a137cfa2a278c0bbb29c3ad5895cf
land-speed <oliver.kelly1010@gmail.com>
2021-06-26 18:27:47 +0100

    First Commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const homeRoutes = require('./routes/home');
+const { nextTick } = require('process');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(homeRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
---
+++ src/routes/home.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const express = require('express');
+const path = require("path");
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'home.html'))
+});
+
+module.exports = router;
