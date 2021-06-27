36b1c634cddafd9b463418562ce7a62374c68747
land-speed <oliver.kelly1010@gmail.com>
2021-06-27 15:16:48 +0100

    added https config

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const http = require('http');
+const https = require('https');
 
 const homeRoutes = require('./routes/home');
 const { nextTick } = require('process');
+
+var privateKey  = fs.readFileSync('../keys/ssl-key.pem', 'utf8');
+var certificate = fs.readFileSync('../keys/sslcert.crt', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
 
 const hostname = '0.0.0.0';
 const port = 8080;
@@ -19,4 +26,8 @@ app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.listen(port, hostname);
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(8080);
+httpsServer.listen(8443);
